a0268bfa558a3934cda265fa376c43b10b6401b0
Azam Gohar <144214140+azamgohar@users.noreply.github.com>
2026-05-09 15:59:35 +0500

    Add files via upload

---
+++ typeof and instanceof/script.js
@@ -0,0 +1,17 @@
+// typeof ✅
+// It uses to know the value's type for example:
+// typeof 5 = Number;
+// typeof "5" = String;
+// but it tells the NaN's value type as a Number and an Arrays's value type as an object which are it's quirks
+
+// instanceof ✅
+// It uses to know that does the value belong to the given value-type or not Example:
+
+// let a = [];
+// a instanceof Array // true, herr because a is belong to an array.
+
+// let a = {}
+
+// a instanceof Array //false, because here a belongs to an object. Not an Array.
+
+
